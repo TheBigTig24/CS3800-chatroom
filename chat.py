74c01adf253a6741e9ece74c3ac57a7936f5c6e2
Caleb C <kaichun24@gmail.com>
2025-04-30 18:44:14 -0700

    added da afk feature

--- chat.py
+++ chat.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import datetime
+import time
 
 host = socket.gethostname()
 port = 5000
@@ -16,6 +18,7 @@ clients = []
 names = []
 
 # stores users time of most recent message sent
+# stores maps with keys: user, time, message
 lastActivity = []
 
 # send msg to all clients
@@ -38,12 +41,16 @@ def manageClient(client):
             messageDecode = message.decode()
             msg = messageDecode.split(' ', 3)
             if msg[1] == '/r':
+                # private message
                 if len(msg) == 4:
                     recipient = msg[2]
                     privateMsg = msg[3]
                     if recipient in names:
                         index = names.index(recipient)
                         clients[index].send((f"{names[clients.index(client)]} is hitting u up privately: {privateMsg}").encode())
+                        # update last activity
+                        activity = {'user': names[clients.index(client)], 'time': datetime.datetime.now(), 'message': privateMsg}
+                        lastActivity.append(activity)
                     else:
                         client.send(f"User {recipient} not found.".encode())
             elif msg[0] == 'admin:' and msg[1] == '/kick':
@@ -53,12 +60,21 @@ def manageClient(client):
                     index = names.index(thePersonToKick)
                     clients[index].send('you have been kicked by admin'.encode())
                     removeClient(clients[index])
+                    # update last activity
+                    activity = {'user': names[clients.index(client)], 'time': datetime.datetime.now(), 'message': f'kicked {thePersonToKick}'}
+                    lastActivity.append(activity)
                 else:
                     client.send(f"User {thePersonToKick} not found.".encode())
             else:
                 # send msg to all other clients
                 flood(message)
-            # flood(message)
+                # update last activity
+                combinedMsg = ''
+                for index in range(1, len(msg)):
+                    combinedMsg += msg[index]
+                activity = {'user': names[clients.index(client)], 'time': datetime.datetime.now(), 'message': combinedMsg}
+                lastActivity.append(activity)
+                
             
         except Exception as e:
             # error , rm client
@@ -75,6 +91,46 @@ def removeClient(client):
     leavingMsg = 'ikiag, ' + name + ' skedaddled, ts so kevin'
     flood(leavingMsg.encode())
     
+    
+# start the inactivity-checker-inator
+def i_call_this_the_inactivity_checker_inator():
+    while True:
+        # check for inactivity every 5 seconds
+        time.sleep(5)
+        
+        # this if statement is to prevent kicking users on join since they have no previous messages
+        if len(lastActivity) != 0:
+            # check people that have sent a message before
+            thesePeopleHaveSpoken = [activity['user'] for activity in lastActivity]
+            
+            # check if they have been active in the last 30 seconds
+            afkers = names.copy()
+            currentTime = datetime.datetime.now()
+            for activity in lastActivity:
+                currName = activity['user']
+                lastTime = activity['time']
+                timeDiff = currentTime - lastTime
+                if timeDiff.total_seconds() < 30:
+                    # remove users that have been active in the last 30 seconds and have typed at least one message
+                    if (currName in afkers) and (currName in thesePeopleHaveSpoken):
+                        afkers.remove(currName)
+                    
+            # remove all users that are inactive for more than 30 seconds
+            afkers = list(set(afkers))
+            for stupidMoron in afkers:
+                try:
+                    index = names.index(stupidMoron)
+                    clients[index].send('you have been away from the k for too long, ur gone buddy'.encode())
+                    removeClient(clients[index])
+                    lastActivity[:] = [activity for activity in lastActivity if activity['user'] != stupidMoron]
+                except Exception as e:
+                    print(f"Error removing client {stupidMoron}: {e}")
+
+# start the inactivity checker thread
+inactivity_thread = threading.Thread(target = i_call_this_the_inactivity_checker_inator)
+inactivity_thread.daemon = True
+inactivity_thread.start()            
+            
 # ts loop is for accepting multiple client connections
 while True:
     client, addr = server.accept()
@@ -122,4 +178,4 @@ while True:
         
         # create new thread for each client
         thread = threading.Thread(target = manageClient, args = (client,))
-        thread.start()
+        thread.start() 
